37695675a0f8e8c7281b7abe3ea9a4bf37a84fd4
Divyam Singh Rauthan <divyamrauthan002@gmail.com>
2026-08-04 10:35:00 +0530

    feat: mongoDb and mongoose setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+connectDB();
+
+// Start the Express server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}. http://localhost:${PORT}`);
 });
