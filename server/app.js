cd81dfc3ae011be493f35bb93880dbb1d891ffd2
Divyam Singh Rauthan <divyamrauthan002@gmail.com>
2026-08-04 10:14:24 +0530

    feat: install dependencies and set up Express server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Inventory Management API is running",
+  });
+});
+
+module.exports = app;
